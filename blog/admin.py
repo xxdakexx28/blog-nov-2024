7d52fe32bb5e64c4292d2c6794aab3068bc3835e
xxdakexx28 <bajormi.2807@gmail.com>
2024-11-23 12:03:19 -0400

    blog-app

---
+++ blog/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Post
+
+# Register your models here.
+class PostAdmin(admin.ModelAdmin):
+   list_display = ('title', 'created_at', 'author')
+   list_filter = ('author','created_at')
+   search_fields = ['title', 'content', 'author']
+  
+
+admin.site.register(Post, PostAdmin)
---
+++ blog/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    
+
+    def __str__(self):
+        return self.title
+
+    
