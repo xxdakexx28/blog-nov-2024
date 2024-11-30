4711ba0a060586c1bf1dcb4eaf981458fbcb34bf
xxdakexx28 <bajormi.2807@gmail.com>
2024-11-30 12:08:11 -0400

    new post

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from . views import PostListView, PostDetailView
+from . views import PostListView, PostDetailView, PostCreateView
 import pickle as pk
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/new/', PostCreateView.as_view(), name='post-new'),
     ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView 
 from .models import Post
 # Create your views here.
 
@@ -8,6 +9,14 @@ class PostListView(ListView):
     template_name = 'post_list.html'
 class PostDetailView(DetailView):
     model = Post
-    template_name = 'post_detail.html'    
+    template_name = 'post_detail.html'  
+
+class PostCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html' 
+    fields = ['title', 'content', 'author']
+
+
+
 
 
