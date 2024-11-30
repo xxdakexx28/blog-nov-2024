019586e02d1e46ef2857f2edeee6b36871be55cd
xxdakexx28 <bajormi.2807@gmail.com>
2024-11-30 11:05:20 -0400

    post detail

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from . views import PostListView
+from . views import PostListView, PostDetailView
+import pickle as pk
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
-]
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    ]
--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Post
 # Create your views here.
 
 class PostListView(ListView):
     model = Post
     template_name = 'post_list.html'
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'    
 
 
