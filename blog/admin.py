36248e5eaea6833ea40da5978fa8da8bc6b7a8df
xxdakexx28 <bajormi.2807@gmail.com>
2024-11-30 09:20:08 -0400

    update admin

--- blog/admin.py
+++ blog/admin.py
@@ -5,7 +5,7 @@ from .models import Post
 class PostAdmin(admin.ModelAdmin):
    list_display = ('title', 'created_at', 'author')
    list_filter = ('author','created_at')
-   search_fields = ['title', 'content', 'author']
+   search_fields = ['title', 'content', 'author__username']
   
 
 admin.site.register(Post, PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -10,6 +10,5 @@ class Post(models.Model):
     
 
     def __str__(self):
-        return self.title
-
+        return self.title 
     
